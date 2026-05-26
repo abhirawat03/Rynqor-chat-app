ee4d3ffee231c20ea0877a760a39cc9cc4a72765
abhirawat03 <abhirawat8076@gmail.com>
2026-05-26 21:22:59 +0530

    Refactor CORS configuration to use an array for allowed origins in app.js and socket/index.js

--- server/src/app.js
+++ server/src/app.js
@@ -7,7 +7,10 @@ const app = express();
 
 app.use(
     cors({
-        origin:"http://localhost:5173",
+        origin:[
+      "http://localhost:5173",
+      "https://rynqor.vercel.app/",
+    ],
         credentials:true,
     })
 )
--- server/src/socket/index.js
+++ server/src/socket/index.js
@@ -5,7 +5,10 @@ import { socketAuth } from "./socketAuth.js";
 const initSocket = (server) => {
     const io = new Server(server, {
         cors: {
-            origin: "http://localhost:5173",
+            origin: [
+      "http://localhost:5173",
+      "https://rynqor.vercel.app/",
+    ],
             credentials: true,
         },
     });
