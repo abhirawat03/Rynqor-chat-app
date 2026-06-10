556894d21773bc813ccdedc0e8442f9058b7cb7c
abhirawat03 <abhirawat8076@gmail.com>
2026-06-10 23:09:49 +0530

    feat: add Dockerfile and .dockerignore; implement zod validation schemas for user login

--- server/src/routes/auth.route.js
+++ server/src/routes/auth.route.js
@@ -1,14 +1,13 @@
 import {Router} from "express"
 import {register, login, getCurrentUser, refreshAccessToken, logout, getSessions, logoutSession} from "../controllers/auth.controller.js"
-import { validateLogin } from "../validations/auth.validation.js";
 import { verifyJwt } from "../middleware/verifyJwt.middleware.js";
 import { validate } from "../middleware/validate.middleware.js";
-import {registerSchema} from "../schemas/auth.schema.js"
+import {registerSchema, loginSchema} from "../schemas/auth.schema.js"
 
 const router = Router();
 
 router.route("/register").post(validate(registerSchema), register);
-router.route("/login").post(validateLogin, login);
+router.route("/login").post(validate(loginSchema), login);
 router.route("/refresh-token").post(refreshAccessToken);
 
 router.route("/current-user").get(verifyJwt, getCurrentUser);
--- server/src/schemas/auth.schema.js
+++ server/src/schemas/auth.schema.js
@@ -1,21 +1,23 @@
 import {z} from "zod";
 
-export const registerSchema = z.object({
+const registerSchema = z.object({
     username:z
         .string()
         .trim()
         .toLowerCase()
-        .min(5, "Username must be at least 5 characters")
+        .min(3, "Username must be at least 3 characters")
         .max(30, "Username must be at most 30 characters")
         .regex(
             /^[a-zA-Z0-9_]+$/,
             "Username must contain only letters, numbers, and underscores"
         ),
+
     email:z
         .string()
         .trim()
         .email("Invalid email format")
         .transform(value => value.toLowerCase()),
+
     password:z
         .string()
         .min(8, "Password must be at least 8 characters")
@@ -23,9 +25,29 @@ export const registerSchema = z.object({
             /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[\W_])/,
             "Password must contain at least one uppercase letter, one lowercase letter, one number, and one special character"
         ),
+
     fullName:z
         .string()
         .trim()
         .min(2, "Full name is required")
         .max(50, "fullName must be at most 50 characters")
 })
+
+const loginSchema = z.object({
+    login: z
+        .string()
+        .trim()
+        .min(
+            3,
+            "Email or username is required"
+        ),
+
+    password: z
+        .string()
+        .min(
+            1,
+            "Password is required"
+        ),
+});
+
+export {registerSchema, loginSchema}
